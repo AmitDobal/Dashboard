141df4df67313103e5af0f895ba122bfcd85a988
Amit Dobal <2019pte5188@mnit.ac.in>
2023-11-24 01:03:25 +0530

    Added app drawer

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidenav from "../components/Sidebnav";
+import { Box } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <Sidenav />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <h1>Home</h1>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
